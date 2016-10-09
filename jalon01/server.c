e0670b6fb12dfd0ee75fd423496c21e966820d0a
Taknok* <pg.developper.fr@gmail.com>
2016-10-09 14:10:37 +0200

    mise a jour du jalon 1 qui bugguais : le revieve list 2047 octets plutot que length(buff)

--- jalon01/server.c
+++ jalon01/server.c
@@ -89,7 +89,7 @@ int do_accept(int sock, struct sockaddr_in * adr){
 
 void do_read(int sockfd, char* buffer){
     memset(buffer, 0, strlen(buffer)); //on s'assure d'avoir des valeurs nulles dans le buff
-    int length_r_buff = recv(sockfd, buffer, strlen(buffer) -1, 0); //rempli le buffer
+    int length_r_buff = recv(sockfd, buffer, 2047, 0); //rempli le buffer
     //si erreur affiche l'erreur, sinon affiche le message envoyé par le client DANS le terminal du serveur (pour check)
     if (length_r_buff < 0) {
         printf("erreur rien n'a été recu\n");
