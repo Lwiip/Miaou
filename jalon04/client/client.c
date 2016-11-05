8f5f7e5040d5649b00c86eaa47fdcc62b6e489a2
Lwiip <louisgalava.net@gmail.com>
2016-11-05 15:46:15 +0100

    change tab

--- jalon04/client/client.c
+++ jalon04/client/client.c
@@ -12,155 +12,155 @@
 // #include "../commons/config.h" pas besoin car cet include est deja dans client.h
 
 struct sockaddr_in do_connect(int sock, struct sockaddr_in sock_host, char* hostname, int port){
-        //reinitialise la memoire
-        memset(&sock_host, 0, sizeof(sock_host));
+    //reinitialise la memoire
+    memset(&sock_host, 0, sizeof(sock_host));
 
-        sock_host.sin_family = AF_INET;
-        inet_aton(hostname, &sock_host.sin_addr);
-        sock_host.sin_port = htons(port);
+    sock_host.sin_family = AF_INET;
+    inet_aton(hostname, &sock_host.sin_addr);
+    sock_host.sin_port = htons(port);
 
-        //check de l'erreur
-        if (connect(sock, (struct sockaddr *) &sock_host, sizeof(sock_host)) == -1) {
-                perror("erreur connect");
-                exit(-1);
-        }
-        return sock_host;
+    //check de l'erreur
+    if (connect(sock, (struct sockaddr *) &sock_host, sizeof(sock_host)) == -1) {
+        perror("erreur connect");
+        exit(-1);
+    }
+    return sock_host;
 }
 
 int read_line(char *text){
-        //recupere la saisie utilisateur (max de BUFFER_SIZE)
+    //recupere la saisie utilisateur (max de BUFFER_SIZE)
 
-        printf("Votre message : ");
-        fgets(text, BUFFER_SIZE, stdin);
+    printf("Votre message : ");
+    fgets(text, BUFFER_SIZE, stdin);
 
 
-        if (strlen(text) > 1) {
-                return 1;
-        } else {
-                return 0;
-        }
+    if (strlen(text) > 1) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 void do_read(int sock, char * text){
-        memset(text, 0, BUFFER_SIZE); //On s'assure que text vaut rien
-        int length_r_buff = recv(sock, text, strlen(text) - 1, 0);
+    memset(text, 0, BUFFER_SIZE); //On s'assure que text vaut rien
+    int length_r_buff = recv(sock, text, strlen(text) - 1, 0);
 
-        //gere l'erreur, si pas afficher le message
-        if (0 >= length_r_buff) {
-                perror("erreur lors de la reception");
-        } else {
-                text[length_r_buff] = '\0';
-                printf("%c[2K", 27); // efface la ligne dans la console
-                fputs(text, stdout);
-        }
+    //gere l'erreur, si pas afficher le message
+    if (0 >= length_r_buff) {
+        perror("erreur lors de la reception");
+    } else {
+        text[length_r_buff] = '\0';
+        printf("%c[2K", 27); // efface la ligne dans la console
+        fputs(text, stdout);
+    }
 }
 
 handle_client_message(int sock, char * text){
-        //envoie des messages et gere l'erreur
-        do_write(sock, text);
-        //ecoute le message
-        do_read(sock, text);
+    //envoie des messages et gere l'erreur
+    do_write(sock, text);
+    //ecoute le message
+    do_read(sock, text);
 }
 
 int main(int argc,char** argv)
 {
-        //gere le cas ou aucun argument n'est passé (ou pas assez)
-        if (argc != 3)
-        {
-                fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
-                return 1;
+    //gere le cas ou aucun argument n'est passé (ou pas assez)
+    if (argc != 3)
+    {
+        fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
+        return 1;
+    }
+
+    struct sockaddr_in sock_host;
+    int sock;
+    fd_set fds;
+
+    //get address info from the server
+    char* hostname = argv[1];
+    //get port
+    int port = atoi(argv[2]);
+    // get_addr_info()
+
+    // Variables pour l'utilisateur coté client
+    char user_name[BUFFER_SIZE];
+    strcpy(user_name, ""); //met le nom vide
+    int registered = FALSE;
+
+    //get the socket
+    sock = do_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    //connect to remote socket
+    sock_host = do_connect(sock, sock_host, hostname, port);
+
+
+    //get user input
+    char *text = malloc (sizeof (*text) * BUFFER_SIZE);
+
+    // nonblock(NB_ENABLE);
+
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+    FD_SET(sock, &fds);
+
+    for(;; ) {
+        // FD_ZERO(&fds);
+        // FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+        // FD_SET(sock, &fds);
+
+        // select(sock+1, &fds, NULL, NULL, 0);
+
+
+        if (FD_ISSET(STDIN_FILENO, &fds)) { //si la modification est faite sur stdin
+            // read_line(text);
+            memset(text, 0, BUFFER_SIZE);
+            // fgets(text, BUFFER_SIZE, stdin);
+            read(STDIN_FILENO, text, BUFFER_SIZE);
+
+
+            if (strcmp(text, "/q\n\0") == 0) {
+                while(-1 == send(sock, text, BUFFER_SIZE, 0)) {
+                    perror("erreur lors de l'envoie");
+                }
+                printf("Deconnection\n");
+                break;
+            }
+
+            // send message to the server
+            // handle_client_message(sock, text);
+            do_write(sock, text);
         }
 
-        struct sockaddr_in sock_host;
-        int sock;
-        fd_set fds;
+        if (!registered) {
+            char local_copy_text[BUFFER_SIZE];
+            char * copy_text = local_copy_text;
+            strcpy(copy_text, text);
 
-        //get address info from the server
-        char* hostname = argv[1];
-        //get port
-        int port = atoi(argv[2]);
-        // get_addr_info()
+            char * commande = strsep(&copy_text, " ");
 
-        // Variables pour l'utilisateur coté client
-        char user_name[BUFFER_SIZE];
-        strcpy(user_name, ""); //met le nom vide
-        int registered = FALSE;
+            if (strcmp(commande, COMMAND_NICK) == 0) {
+                copy_text[strlen(copy_text) - 1] = '\0';
 
-        //get the socket
-        sock = do_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+                registered = TRUE;
+                snprintf(user_name, BUFFER_SIZE, "%s", copy_text);
+            }
+        }
 
-        //connect to remote socket
-        sock_host = do_connect(sock, sock_host, hostname, port);
-
-
-        //get user input
-        char *text = malloc (sizeof (*text) * BUFFER_SIZE);
-
-        // nonblock(NB_ENABLE);
 
         FD_ZERO(&fds);
         FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
         FD_SET(sock, &fds);
+        printf("%s > ", user_name);
+        fflush(stdout);
 
-        for(;; ) {
-                // FD_ZERO(&fds);
-                // FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
-                // FD_SET(sock, &fds);
+        select(sock+1, &fds, NULL, NULL, 0);
 
-                // select(sock+1, &fds, NULL, NULL, 0);
+        if (FD_ISSET(sock, &fds)) { //si la modification est faite sur l'ecoute
+        do_read(sock, text);
+    }
+}
 
-
-                if (FD_ISSET(STDIN_FILENO, &fds)) { //si la modification est faite sur stdin
-                        // read_line(text);
-                        memset(text, 0, BUFFER_SIZE);
-                        // fgets(text, BUFFER_SIZE, stdin);
-                        read(STDIN_FILENO, text, BUFFER_SIZE);
-
-
-                        if (strcmp(text, "/q\n\0") == 0) {
-                                while(-1 == send(sock, text, BUFFER_SIZE, 0)) {
-                                        perror("erreur lors de l'envoie");
-                                }
-                                printf("Deconnection\n");
-                                break;
-                        }
-
-                        // send message to the server
-                        // handle_client_message(sock, text);
-                        do_write(sock, text);
-                }
-
-                if (!registered) {
-                        char local_copy_text[BUFFER_SIZE];
-                        char * copy_text = local_copy_text;
-                        strcpy(copy_text, text);
-
-                        char * commande = strsep(&copy_text, " ");
-
-                        if (strcmp(commande, COMMAND_NICK) == 0) {
-                                copy_text[strlen(copy_text) - 1] = '\0';
-
-                                registered = TRUE;
-                                snprintf(user_name, BUFFER_SIZE, "%s", copy_text);
-                        }
-                }
-
-
-                FD_ZERO(&fds);
-                FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
-                FD_SET(sock, &fds);
-                printf("%s > ", user_name);
-                fflush(stdout);
-
-                select(sock+1, &fds, NULL, NULL, 0);
-
-                if (FD_ISSET(sock, &fds)) { //si la modification est faite sur l'ecoute
-                        do_read(sock, text);
-                }
-        }
-
-        free(text);
-        close(sock);
-        // nonblock(NB_DISABLE);
-        return 0;
+free(text);
+close(sock);
+// nonblock(NB_DISABLE);
+return 0;
 }
--- jalon04/server/server_app.c
+++ jalon04/server/server_app.c
@@ -18,141 +18,141 @@
 // #include "server.h" //commande.h include deja sever.h
 
 void init_message(Message * message){
-        message->buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
-        message->dest_name = (char *)malloc(BUFFER_SIZE * sizeof(char));
+    message->buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
+    message->dest_name = (char *)malloc(BUFFER_SIZE * sizeof(char));
 }
 void free_message(Message * message){
-        free(message->buffer);
-        free(message->dest_name);
+    free(message->buffer);
+    free(message->dest_name);
 }
 
 void init_serv_addr(int port, struct sockaddr_in * serv_addr) {
 
-        //clean the serv_add structure
-        memset(serv_addr, 0, sizeof(serv_addr));
+    //clean the serv_add structure
+    memset(serv_addr, 0, sizeof(serv_addr));
 
-        //cast the port from a string to an int
-        //portno = atoi(port); osef car on a mis dans une constante ici coté serveur
+    //cast the port from a string to an int
+    //portno = atoi(port); osef car on a mis dans une constante ici coté serveur
 
-        //internet family protocol
-        serv_addr->sin_family = AF_INET;
+    //internet family protocol
+    serv_addr->sin_family = AF_INET;
 
-        //we bind to any ip form the host
-        serv_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+    //we bind to any ip form the host
+    serv_addr->sin_addr.s_addr = htonl(INADDR_ANY);
 
-        //we bind on the tcp port specified
-        serv_addr->sin_port = htons(port);
+    //we bind on the tcp port specified
+    serv_addr->sin_port = htons(port);
 
 }
 
 void do_bind(int sock, struct sockaddr_in adr){
 
-        int retour = bind(sock, (struct sockaddr *) &adr, sizeof(adr));
-        if( retour == -1 ) {
-                printf("%i\n",errno );
-                perror("erreur lors du bind");
-        }
+    int retour = bind(sock, (struct sockaddr *) &adr, sizeof(adr));
+    if( retour == -1 ) {
+        printf("%i\n",errno );
+        perror("erreur lors du bind");
+    }
 }
 
 void do_listen(int sock){
-        if( listen( sock, SOMAXCONN) == -1 ) {
-                perror("erreur lors de l'ecoute");
-                exit(EXIT_FAILURE);
-        }
+    if( listen( sock, SOMAXCONN) == -1 ) {
+        perror("erreur lors de l'ecoute");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int do_accept(int sock, struct sockaddr_in * adr){
-        int addrlen=sizeof(adr);
-        int new_sock=accept(sock, (struct sockaddr *) &adr,&addrlen);
-        if(new_sock==-1)
-                printf("Desole, je ne peux pas accepter la session TCP\n");
-        else
-                printf("connection      : OK\n");
-        return new_sock;
+    int addrlen=sizeof(adr);
+    int new_sock=accept(sock, (struct sockaddr *) &adr,&addrlen);
+    if(new_sock==-1)
+    printf("Desole, je ne peux pas accepter la session TCP\n");
+    else
+    printf("connection      : OK\n");
+    return new_sock;
 
 }
 
 int do_read(Message * message){
-        memset(message->buffer, 0, BUFFER_SIZE); //on s'assure d'avoir des valuers nulles dans le buff
-        int length_r_buff = recv((message->sender).lst_sock, message->buffer, BUFFER_SIZE -1, 0);
+    memset(message->buffer, 0, BUFFER_SIZE); //on s'assure d'avoir des valuers nulles dans le buff
+    int length_r_buff = recv((message->sender).lst_sock, message->buffer, BUFFER_SIZE -1, 0);
 
-        if (length_r_buff < 0) {
-                printf("erreur rien n'a été recu\n");
-        } else {
-                message->buffer[length_r_buff] = '\0';
-        }
-        return length_r_buff;
+    if (length_r_buff < 0) {
+        printf("erreur rien n'a été recu\n");
+    } else {
+        message->buffer[length_r_buff] = '\0';
+    }
+    return length_r_buff;
 }
 
 void do_send(Message message, Client * liste_clients, int compteur){
-        int i = 0;
-        switch (message.destination) {
+    int i = 0;
+    switch (message.destination) {
         case no_one:;
-                do_write((message.sender).lst_sock, message.buffer);
-                break;
+        do_write((message.sender).lst_sock, message.buffer);
+        break;
 
         case everyone:;  //everyone
-                for (i = 0; i < compteur; ++i) {
-                        if ((message.sender).lst_sock != liste_clients[i].lst_sock) { //on n'envoie pas au sender
-                                do_write(liste_clients[i].lst_sock, message.buffer);
-                        }
-                }
-                break;
-
-        case user:;
-                for (i = 0; i < compteur; ++i) {
-                        if (strcmp(liste_clients[i].pseudo, message.dest_name) == 0) { //on envoie qu'a la dest
-                                do_write(liste_clients[i].lst_sock, message.buffer);
-                                return; //sort de la fonction
-                        }
-                }
-                break;
-
-        default:
-                perror("Aucune destination pour ce message");
-                break;
+        for (i = 0; i < compteur; ++i) {
+            if ((message.sender).lst_sock != liste_clients[i].lst_sock) { //on n'envoie pas au sender
+            do_write(liste_clients[i].lst_sock, message.buffer);
         }
+    }
+    break;
+
+    case user:;
+    for (i = 0; i < compteur; ++i) {
+        if (strcmp(liste_clients[i].pseudo, message.dest_name) == 0) { //on envoie qu'a la dest
+        do_write(liste_clients[i].lst_sock, message.buffer);
+        return; //sort de la fonction
+    }
+}
+break;
+
+default:
+perror("Aucune destination pour ce message");
+break;
+}
 }
 
 
 void clear_clients(Client *liste_clients, int compteur)
 {
-        int i = 0;
-        for(i = 0; i < compteur; i++)
-        {
-                close(liste_clients[i].lst_sock);
-        }
+    int i = 0;
+    for(i = 0; i < compteur; i++)
+    {
+        close(liste_clients[i].lst_sock);
+    }
 }
 
 int remove_client(Client * liste_clients, int i, int compteur){
-        /*supprimer le i ème client et decroit le compteur de client*/
-        int k = 0;
-        int w = k;
+    /*supprimer le i ème client et decroit le compteur de client*/
+    int k = 0;
+    int w = k;
 
-        for (k; k < compteur -1; k++) { //jusqu'a compteur -1 car on suppr une personne
-                if (k == i) {
-                        w++;
-                        free(liste_clients[i].pseudo);
-                }
-                liste_clients[k] = liste_clients[w];
-                w++;
-        }
+    for (k; k < compteur -1; k++) { //jusqu'a compteur -1 car on suppr une personne
+    if (k == i) {
+        w++;
+        free(liste_clients[i].pseudo);
+    }
+    liste_clients[k] = liste_clients[w];
+    w++;
+}
 
-        return --compteur;
+return --compteur;
 }
 
 /*
-   a exporter pour commands
- */
+a exporter pour commands
+*/
 int get_indice_user(Client * liste_clients, char * client_pseudo, int compteur){
-        int i = 0;
-        for (i = 0; i < compteur; ++i)
-        {
-                if (strcmp(liste_clients[i].pseudo, client_pseudo) == 0) {
-                        return i;
-                }
+    int i = 0;
+    for (i = 0; i < compteur; ++i)
+    {
+        if (strcmp(liste_clients[i].pseudo, client_pseudo) == 0) {
+            return i;
         }
-        return -1; //on ne l'a pas trouvé
+    }
+    return -1; //on ne l'a pas trouvé
 
 }
 
@@ -160,18 +160,18 @@ int get_indice_user(Client * liste_clients, char * client_pseudo, int compteur){
 
 void get_ip_port_client(int rep_sock, Client * liste_clients, int compteur){
 
-        struct sockaddr_in sin;
-        socklen_t len = sizeof(sin);
+    struct sockaddr_in sin;
+    socklen_t len = sizeof(sin);
 
-        if (getpeername(rep_sock, (struct sockaddr *)&sin, &len) == -1) {
-                perror("getpeername");
-        } else {
-                liste_clients[compteur].port = ntohs(sin.sin_port);
-        }
+    if (getpeername(rep_sock, (struct sockaddr *)&sin, &len) == -1) {
+        perror("getpeername");
+    } else {
+        liste_clients[compteur].port = ntohs(sin.sin_port);
+    }
 
-        struct sockaddr_in* pV4Addr = (struct sockaddr_in*)&sin;
-        struct in_addr ipAddr = pV4Addr->sin_addr;
-        inet_ntop( AF_INET, &ipAddr, liste_clients[compteur].ip, INET_ADDRSTRLEN );
+    struct sockaddr_in* pV4Addr = (struct sockaddr_in*)&sin;
+    struct in_addr ipAddr = pV4Addr->sin_addr;
+    inet_ntop( AF_INET, &ipAddr, liste_clients[compteur].ip, INET_ADDRSTRLEN );
 }
 
 
@@ -183,129 +183,129 @@ void get_ip_port_client(int rep_sock, Client * liste_clients, int compteur){
 int main(int argc, char** argv){
 
 
-        /*
-           Initialisation variables
-         */
-        if (argc != 2) {
-                fprintf(stderr, "usage: RE216_SERVER port\n");
-                return 1;
+    /*
+    Initialisation variables
+    */
+    if (argc != 2) {
+        fprintf(stderr, "usage: RE216_SERVER port\n");
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+
+    int compteur=0;
+
+    Client liste_clients[MAX_CLIENTS];
+    Message message;
+    init_message(&message);
+
+    struct sockaddr_in serv_addr;
+
+    fd_set readfds;
+    fd_set readfds2;
+
+    int lst_sock = do_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int max = lst_sock;
+
+
+
+    /*
+    Debut code
+    */
+
+    /*Initialisation du serveur*/
+
+    //init the serv_add structure
+    init_serv_addr(port, &serv_addr);
+
+    //perform the binding
+    //we bind on the tcp port specified
+    do_bind(lst_sock, serv_addr);
+
+    //specify the socket to be a server socket and listen for at most 20 concurrent client
+    do_listen(lst_sock);
+
+    int a = 0;
+    /*Boucle du serveur*/
+    for (;; ) {
+        int i = 0;
+        //on vide l'ensemble readfds
+        FD_ZERO(&readfds);
+
+        //on ajoute la socket
+        FD_SET(lst_sock, &readfds);
+
+        //parcourt de la liste_clients et ajout des sockets d'écoute dans le fd_set
+        for(i = 0; i < compteur; i++) {
+            FD_SET(liste_clients[i].lst_sock, &readfds);
         }
 
-        int port = atoi(argv[1]);
-
-        int compteur=0;
-
-        Client liste_clients[MAX_CLIENTS];
-        Message message;
-        init_message(&message);
-
-        struct sockaddr_in serv_addr;
-
-        fd_set readfds;
-        fd_set readfds2;
-
-        int lst_sock = do_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        int max = lst_sock;
-
-
-
-        /*
-           Debut code
-         */
-
-        /*Initialisation du serveur*/
-
-        //init the serv_add structure
-        init_serv_addr(port, &serv_addr);
-
-        //perform the binding
-        //we bind on the tcp port specified
-        do_bind(lst_sock, serv_addr);
-
-        //specify the socket to be a server socket and listen for at most 20 concurrent client
-        do_listen(lst_sock);
-
-        int a = 0;
-        /*Boucle du serveur*/
-        for (;; ) {
-                int i = 0;
-                //on vide l'ensemble readfds
-                FD_ZERO(&readfds);
-
-                //on ajoute la socket
-                FD_SET(lst_sock, &readfds);
-
-                //parcourt de la liste_clients et ajout des sockets d'écoute dans le fd_set
-                for(i = 0; i < compteur; i++) {
-                        FD_SET(liste_clients[i].lst_sock, &readfds);
-                }
-
-                readfds2=readfds;
-                //utilisation de select
-                if(-1 == select(max + 1, &readfds2, 0, 0, 0)) {
-                        perror("erreur dans l'appel a select()");
-                        exit(errno);
-                }
-
-                // Si il y a eu un chgt sur la socket principal d'écoute
-                if(FD_ISSET(lst_sock, &readfds2)) {
-                        /*ajoute un client a la liste*/
-                        int rep_sock = do_accept(lst_sock,&serv_addr); //accept la connexion
-
-
-
-                        printf("\nNouveau client : %i\n", rep_sock);
-
-                        //Calcul du max
-                        if (rep_sock>max) {
-                                max=rep_sock;
-                        } else {
-                                max=max;
-                        }
-
-
-
-                        //ajout de la nouvelle socket client a l'ecoute
-                        FD_SET(rep_sock, &readfds2);
-
-
-                        liste_clients[compteur].lst_sock = rep_sock; //sauvegarde du client
-                        liste_clients[compteur].registered = 0; //le client n'est pas encore enregistre
-                        liste_clients[compteur].connection_date = time(NULL);
-                        liste_clients[compteur].user_channel = NULL;
-                        get_ip_port_client(rep_sock, liste_clients, compteur);
-
-                        printf("%s %i\n", liste_clients[compteur].ip, liste_clients[compteur].port);
-
-
-                        compteur++; //incremente le nb de client que l'on possède
-                } else {
-
-                        int i = 0;
-                        for(i = 0; i < compteur; i++) {
-                                /* un client ecrit */
-                                if(FD_ISSET(liste_clients[i].lst_sock, &readfds2)) {
-
-                                        message.destination = no_one; //par defaut on envoie a personne
-                                        message.sender = liste_clients[i];
-                                        int retour_client = do_read(&message); //ecoute ce que le client envoie
-                                        commande_quit("", retour_client, liste_clients, i, &compteur, &readfds2); //check crash client (ctrl + c)
-
-                                        if (strlen(message.buffer) != 0) { //evite le double select et le cas ou l'utilisateur envoie rien
-
-                                                printf("Entrée : %s", message.buffer);
-
-                                                if (do_commande(&message, retour_client, liste_clients, i, &compteur, &readfds2)) {
-                                                        printf("Sortie : %s\n", message.buffer);
-                                                        do_send(message, liste_clients, compteur); //repond
-                                                }
-                                                break;
-                                        }
-                                }
-                        }
-                }
-                a++;
+        readfds2=readfds;
+        //utilisation de select
+        if(-1 == select(max + 1, &readfds2, 0, 0, 0)) {
+            perror("erreur dans l'appel a select()");
+            exit(errno);
         }
 
-        clear_clients(liste_clients, compteur); //on suprime tout si on ferme le serveur
+        // Si il y a eu un chgt sur la socket principal d'écoute
+        if(FD_ISSET(lst_sock, &readfds2)) {
+            /*ajoute un client a la liste*/
+            int rep_sock = do_accept(lst_sock,&serv_addr); //accept la connexion
+
+
+
+            printf("\nNouveau client : %i\n", rep_sock);
+
+            //Calcul du max
+            if (rep_sock>max) {
+                max=rep_sock;
+            } else {
+                max=max;
+            }
+
+
+
+            //ajout de la nouvelle socket client a l'ecoute
+            FD_SET(rep_sock, &readfds2);
+
+
+            liste_clients[compteur].lst_sock = rep_sock; //sauvegarde du client
+            liste_clients[compteur].registered = 0; //le client n'est pas encore enregistre
+            liste_clients[compteur].connection_date = time(NULL);
+            liste_clients[compteur].user_channel = NULL;
+            get_ip_port_client(rep_sock, liste_clients, compteur);
+
+            printf("%s %i\n", liste_clients[compteur].ip, liste_clients[compteur].port);
+
+
+            compteur++; //incremente le nb de client que l'on possède
+        } else {
+
+            int i = 0;
+            for(i = 0; i < compteur; i++) {
+                /* un client ecrit */
+                if(FD_ISSET(liste_clients[i].lst_sock, &readfds2)) {
+
+                    message.destination = no_one; //par defaut on envoie a personne
+                    message.sender = liste_clients[i];
+                    int retour_client = do_read(&message); //ecoute ce que le client envoie
+                    commande_quit("", retour_client, liste_clients, i, &compteur, &readfds2); //check crash client (ctrl + c)
+
+                    if (strlen(message.buffer) != 0) { //evite le double select et le cas ou l'utilisateur envoie rien
+
+                    printf("Entrée : %s", message.buffer);
+
+                    if (do_commande(&message, retour_client, liste_clients, i, &compteur, &readfds2)) {
+                        printf("Sortie : %s\n", message.buffer);
+                        do_send(message, liste_clients, compteur); //repond
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    a++;
+}
+
+clear_clients(liste_clients, compteur); //on suprime tout si on ferme le serveur
 }
